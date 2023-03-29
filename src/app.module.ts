4d0dcfea1cdfad3b429a17c5d2c9bff862c9939e
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-03-29 11:52:43 -0300

    feat: initial crud to register users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    UsersModule,
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.cfcdqqw.mongodb.net/?retryWrites=true&w=majority`,
+    ),
+  ],
+  controllers: [UsersController],
+  providers: [UsersService],
 })
 export class AppModule {}
